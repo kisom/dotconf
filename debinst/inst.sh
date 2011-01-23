1318913025443eeb54d752b7cb4e74b079e9fb92
Kyle Isom <coder@kyleisom.net>
2011-01-23 04:56:14 -0700

    work on debinst

--- debinst/inst.sh
+++ debinst/inst.sh
@@ -10,10 +10,16 @@ SOURCES=/etc/apt/sources.list
 TOR="deb http://deb.torproject.org/torproject.org ${NDIST} main"
 NDIST="squeeze"
 ODIST="lenny"
+DEV_USER="kyle"
 
 if [ ! "$UID" = "0" ]; then
     echo "requires root privileges!"
     exit 1
+fi
+
+grep ${DEV_USER} /etc/passwd 2>&1 > /dev/null
+if [ "$?" = "1" ]; then
+    adduser ${DEV_USER}
 fi
 
 # track squeeze
@@ -30,6 +36,29 @@ gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | apt-key add -
 
 up2
 
-# install packages
+# install packages and ensure system is up to date
 apt-get install $(cat debian-install.txt | xargs)
+up2
 
+# change dev user's shell
+usermod -s /bin/zsh ${DEV_USER}
+
+# add user to sudo group
+sed -i -e 's/^\(sudo.\+\)/\1${DEV_USER}/' /etc/group
+
+# clone dotconf into user's dir
+mkdir -p ~${DEV_USER}/code
+cd ~${DEV_USER}
+git clone git://github.com/kisom/dotconf.git
+
+# remove git dir until pubkey added to github account
+rm -fr ~${DEV_USER}/.git
+
+# ensure permissions are set properly
+chown -R ${DEV_USER}:${DEV_USER} ~${DEV_USER}
+chmod u+rw,a-rw,g=r ~{DEV_USER}
+
+
+echo "POST INSTALL TASKS"
+echo "* visudo"
+echo "* user gen pubkey -> github"
