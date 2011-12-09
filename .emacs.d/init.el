bd3c71b2465761a563525adc22f26bae8070b87e
Kyle Isom <coder@kyleisom.net>
2011-12-09 22:17:30 +0300

    add slime loader

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,7 +11,7 @@
 ;; basic setup
 ;; -----------------------------------------------------------------------------
     (add-to-list 'load-path 
-                 "/Users/kyle/emacs.d/site-lisp")
+                 "/Users/kyle/emacs.d/site-lisp/")
 
     (setq display-time-day-and-date 1)
     (setq display-time-24hr-format 1)
@@ -20,6 +20,18 @@
     (setq user-mail-address "coder@kyleisom.net")
     (setq explicit-shell-file-name "/bin/zsh")
     (setq column-number-mode 1)
+    (setq inhibit-startup-message t)
+
+; ------------------------------------------------------------------------------
+; from https://sites.google.com/site/steveyegge2/effective-emacs
+; ------------------------------------------------------------------------------
+; don't rely on meta key, and keep it to one hand
+    (global-set-key "\C-c\C-e" 'execute-extended-command)
+
+; remove scroll
+    (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+    (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+
 
 ;; This was installed by package-install.el.
 ;; This provides support for the package system and
@@ -43,42 +55,9 @@
     (require 'mediawiki)
 
 ;; -----------------------------------------------------------------------------
-;; python support
+;; org-mode setup
 ;; -----------------------------------------------------------------------------
-    (require 'tramp)
-    (require 'compile)
-
-;    (require 'ipython)
-    (require 'comint)
-    (define-key comint-mode-map [(meta p)]
-      'comint-previous-matching-input-from-input)
-    (define-key comint-mode-map [(meta n)]
-      'comint-next-matching-input-from-input)
-    (define-key comint-mode-map [(control meta n)]
-      'comint-next-input)
-    (define-key comint-mode-map [(control meta p)]
-      'comint-previous-input)
-
-    (setq comint-completion-autolist t	;list possibilities on partial
-					;completion
-       comint-completion-recexact nil	;use shortest compl. if
-					;characters cannot be added
-       ;; how many history items are stored in comint-buffers (e.g. pyshell)
-       ;; use the HISTSIZE environment variable that shells use (if avail.)
-       ;; (default is 32)
-       comint-input-ring-size (string-to-number (or (getenv  
-    "HISTSIZE") "100")))
-
-
-    (add-to-list 'interpreter-mode-alist '("ipython" . python-mode))
-    (setq py-python-command-args '("-pylab" "-colors" "LightBG"))
-
-    (autoload 'python-pylint "python-pylint")
-    (autoload 'pylint "python-pylint")
-
-    (autoload 'python-pep8 "python-pep8")
-    (autoload 'pep8 "python-pep8")
-
+    (add-hook 'org-mode-hook 'turn-on-font-lock)
 
 ;; -----------------------------------------------------------------------------
 ;; colour scheme 
@@ -98,3 +77,29 @@
 ;; packages to load on start
 ;; -----------------------------------------------------------------------------
     (load-library "mediawiki")
+
+;; -----------------------------------------------------------------------------
+;; markdown setup
+;; -----------------------------------------------------------------------------
+    (load (expand-file-name "~/.emacs.d/site-lisp/markdown-mode.el"))
+    (require 'markdown-mode)
+    (setq auto-mode-alist (cons '("^.*.md$" . markdown-mode) auto-mode-alist))
+    (add-hook 'markdown-mode-hook '(auto-fill-mode))
+    (add-hook 'markdown-mode-hook '(setq fill-column 76))
+
+;; -------------------------------------------------------=----------------------
+;; import other local files
+;; -----------------------------------------------------------------------------
+    (load (expand-file-name "~/.emacs.d/init-python.el"))
+
+;; -----------------------------------------------------------------------------
+;; slime setup
+;; -----------------------------------------------------------------------------
+    (add-to-list 'load-path 
+		 (expand-file-name "~/src/slime/"))
+    (require 'slime)
+    (add-hook 'lisp-mode-hook (lambda () (slime-mode t)))
+    (add-hook 'inferior-lisp-mode-hook (lambda () (inferior-slime-mode t)))
+    
+    ;; Optionally, specify the lisp program you are using. Default is "lisp"
+    (setq inferior-lisp-program "/usr/local/bin/sbcl") 
