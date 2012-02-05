daf5441b194bc5b869c5482a03f3159619ac6351
Kyle Isom <coder@kyleisom.net>
2012-02-05 18:31:50 +0300

    adding publish, preview, and review alias for site

--- .init_rvm.zsh
+++ .init_rvm.zsh
@@ -1,4 +1,7 @@
 init_rvm () {
     [[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm
+    alias publish='rake generate deploy'
+    alias preview='rake generate preview'
+    alias review='open http://localhost:4000 &'
 }
 
