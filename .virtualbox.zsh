a8ee985f387e657fc7183ac4cdea906488dcc2b2
Kyle Isom <coder@kyleisom.net>
2012-02-05 18:30:57 +0300

    adding virtualbox control

---
+++ .virtualbox.zsh
@@ -0,0 +1,43 @@
+# virtual box code for zsh
+
+vmup () {
+    if [ -z "$1" ]; then
+        print 'no VM specified.'
+        return
+    fi
+ 
+    nohup VBoxHeadless -s "$1" &
+}
+
+vmdown () {
+    if [ -z "$1" ]; then
+        print "no VM specified."
+        return
+    fi
+    VBoxManage controlvm "$1" acpipowerbutton
+}
+
+vmls () {
+    VBoxManage list vms
+}
+
+vmsrun () {
+    VBoxManage list runningvms
+}
+
+vmsuspend () {
+    if [ -z "$1" ]; then
+        return
+    fi
+ 
+    VBoxManage controlvm "$1" savestat 
+}
+
+vmkill () {
+    if [ -z "$1" ]; then
+        return
+    fi
+    VBoxManage controlvm "$1" poweroff
+}
+
+
